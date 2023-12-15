d1e8e6851b51aab36909eed6c9546a75040b8b1d
NdegeFelix <findech6@gmail.com>
2023-12-15 22:01:46 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,32 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    
+    query = session.query(Dog)
+    all_dogs = query.all()
+    return all_dogs
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
+
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter(Dog.id == id).first()
+    return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
